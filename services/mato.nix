b8448330605d5652d86ca5a9a025420ccfeff26d
Kilian Koeltzsch <me@kilian.io>
2025-08-06 11:22:22 +0200

    add mato secrets

--- services/mato.nix
+++ services/mato.nix
@@ -7,11 +7,17 @@ let
       app:
         image: ghcr.io/kiliankoe/mato:latest
         restart: unless-stopped
-        env_file:
-          - .env
         environment:
           - TIMEZONE=Europe/Berlin
           - DB_PATH=/data/db.json
+          - MY_EMAIL=${builtins.readFile config.sops.secrets."mato/my_email".path}
+          - SMTP_HOST=${builtins.readFile config.sops.secrets."mato/smtp_host".path}
+          - SMTP_FROM=${builtins.readFile config.sops.secrets."mato/smtp_from".path}
+          - SMTP_USER=${builtins.readFile config.sops.secrets."mato/smtp_user".path}
+          - SMTP_PASS=${builtins.readFile config.sops.secrets."mato/smtp_pass".path}
+          - CATFACT_SLACK_WEBHOOK_URL=${
+            builtins.readFile config.sops.secrets."mato/catfact_slack_webhook_url".path
+          }
         volumes:
           - mato-data:/data
         ports:
