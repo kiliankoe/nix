36b6b0befd79a22887867cfc1aa228a6e6cc0d4a
Kilian Koeltzsch <me@kilian.io>
2025-08-09 17:33:51 +0200

    add some more base darwin config options

--- modules/darwin/base.nix
+++ modules/darwin/base.nix
@@ -12,41 +12,123 @@
   };
 
   # https://nix-darwin.github.io/nix-darwin/manual/
+  # https://mynixos.com/nix-darwin/options
 
   system.defaults = {
-    # NSGlobalDomain, duh
     NSGlobalDomain = {
-      # AppleHighlightColor = "1.000000 0.749020 0.823529 Pink";
-      NSTableViewDefaultSizeMode = 1; # small sidebar icons
-      NSWindowResizeTime = 0.001;
+      # Use small sidebar icons in lists and outlines
+      NSTableViewDefaultSizeMode = 1;
+      # Disable smart quotes (annoying when typing code)
       NSAutomaticQuoteSubstitutionEnabled = false;
+      # Disable smart dashes (annoying when typing code)
       NSAutomaticDashSubstitutionEnabled = false;
+      # Disable auto-correct
       NSAutomaticSpellingCorrectionEnabled = false;
     };
+
     # com.apple.dock
     dock = {
-      tilesize = 36;
+      # Automatically hide and show the Dock
       autohide = true;
-      showhidden = true; # make icons of hidden applications translucent
-      expose-animation-duration = 0.01;
-      autohide-delay = 0.0;
-      autohide-time-modifier = 0.0;
+      # Change minimize/maximize window effect to scale
+      mineffect = "scale";
     };
+
     # com.apple.finder
     finder = {
-      _FXShowPosixPathInTitle = true;
-      FXDefaultSearchScope = "SCcf"; # search current folder by default
+      # When performing a search, search the current folder by default
+      FXDefaultSearchScope = "SCcf";
+      # Show path bar
       ShowPathbar = true;
-      AppleShowAllExtensions = true;
-      # DisableAllAnimations = true;
-      FXPreferredViewStyle = "clmv"; # column view as default, allowed values: `Nlsv`, `icnv`, `clmv`, `Flwv`
-      # WarnOnEmptyTrash = false;
-      # OpenWindowForNewRemovableDisk = true;
+      # Use column view as default (icnv=Icon, Nlsv=List, clmv=Column, Flwv=Gallery)
+      FXPreferredViewStyle = "clmv";
     };
+
     # com.apple.screencapture
     screencapture = {
+      # Save screenshots to custom directory
       location = "/Users/kilian/Pictures/Screenshots";
+      # Save screenshots in PNG format (current: png)
       type = "png";
+    };
+
+    # Where does this come from?
+    trackpad = {
+      # Enable tap to click for this user and for the login screen
+      Clicking = true;
+      # Enable three finger drag
+      TrackpadThreeFingerDrag = true;
+    };
+  };
+
+  # Custom user preferences for settings not directly supported by nix-darwin
+  system.defaults.CustomUserPreferences = {
+    NSGlobalDomain = {
+      # Set highlight color to pink
+      AppleHighlightColor = "1.000000 0.749020 0.823529 Pink";
+    };
+
+    # Automatically quit printer app once the print jobs complete
+    "com.apple.print.PrintingPrefs"."Quit When Finished" = true;
+
+    "com.apple.screensaver" = {
+      # Require password immediately after sleep or screen saver begins
+      askForPassword = 1;
+      askForPasswordDelay = 0;
+    };
+
+    # Accessibility settings
+    "com.apple.universalaccess" = {
+      # Use scroll gesture with the Ctrl (^) modifier key to zoom
+      closeViewScrollWheelToggle = true;
+      HIDScrollZoomModifierMask = 262144;
+      # Follow the keyboard focus while zoomed in
+      closeViewZoomFollowsFocus = true;
+    };
+
+    # Avoid creating .DS_Store files on network volumes
+    "com.apple.desktopservices".DSDontWriteNetworkStores = true;
+
+    "com.apple.frameworks.diskimages" = {
+      # Automatically open a new Finder window when a read-only volume is mounted
+      auto-open-ro-root = true;
+      # Automatically open a new Finder window when a read-write volume is mounted
+      auto-open-rw-root = true;
+    };
+
+    "com.apple.Safari" = {
+      # Privacy: don't send search queries to Apple
+      UniversalSearchEnabled = false;
+      SuppressSearchSuggestions = true;
+      # Show the full URL in the address bar (note: this still hides the scheme)
+      ShowFullURLInSmartSearchField = true;
+      # Set Safari's home page to `about:blank` for faster loading
+      HomePage = "about:blank";
+      # Prevent Safari from opening 'safe' files automatically after downloading
+      AutoOpenSafeDownloads = false;
+      # Enable Safari's debug menu
+      IncludeInternalDebugMenu = true;
+      # Make Safari's search banners default to Contains instead of Starts With
+      FindOnPageMatchesWordStartsOnly = false;
+      # Enable the Develop menu and the Web Inspector in Safari
+      IncludeDevelopMenu = true;
+      WebKitDeveloperExtrasEnabledPreferenceKey = true;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
+    };
+
+    "com.apple.TextEdit" = {
+      # Use plain text mode for new TextEdit documents
+      RichText = 0;
+      # Open and save files as UTF-8 in TextEdit
+      PlainTextEncoding = 4;
+      PlainTextEncodingForWrite = 4;
+    };
+
+    "com.apple.messageshelper.MessageController".SOInputLineSettings = {
+      # Disable smart quotes as it's annoying for messages that contain code
+      automaticQuoteSubstitutionEnabled = false;
+      # Disable continuous spell checking
+      continuousSpellCheckingEnabled = false;
     };
   };
 
