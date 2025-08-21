6fb0b25459da0fab478a50d3990c54fcdb0efc49
Kilian Koeltzsch <me@kilian.io>
2025-08-21 14:12:10 +0200

    set shared and voyager-specific mas apps

--- hosts/voyager/homebrew.nix
+++ hosts/voyager/homebrew.nix
@@ -21,5 +21,19 @@
       # "wacom-tablet" # install when necessary
       "zoo-design-studio"
     ];
+
+    masApps = {
+      "AusweisApp" = 948660805;
+      "Bluetooth Inspector" = 1509085044;
+      "Compressor" = 424390742;
+      "CustomSymbols" = 1566662030;
+      "Day One" = 1055511498;
+      "Final Cut Pro" = 424389933;
+      "Gifski" = 1351639930;
+      "Logic Pro" = 634148309;
+      "RocketSim" = 1504940162;
+      "Unfolder" = 1410628659;
+      "Yomu" = 562211012;
+    };
   };
 }
--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -28,6 +28,8 @@
       "coconutbattery"
       "crossover"
       "cyberduck"
+      "daisydisk"
+      "devcleaner"
       "discord"
       "element"
       "fantastical"
@@ -65,6 +67,7 @@
       "suspicious-package"
       "tableplus"
       "tailscale-app"
+      "the-unarchiver"
       "todoist-app"
       "utm"
       "visual-studio-code"
@@ -74,5 +77,25 @@
       "zed"
       "zen"
     ];
+
+    masApps = {
+      "Bakery" = 1575220747;
+      "Developer" = 640199958;
+      "Home Assistant" = 1099568401; # is in brew, but more up to date in mas
+      "Keynote" = 409183694;
+      "Lungo" = 1263070803;
+      "Magnet" = 441258766;
+      "Navigator" = 1590354537;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
+      "Reeder" = 1529448980;
+      "Shareful" = 1522267256;
+      "SomaFM" = 449155338;
+      "Speediness" = 1596706466;
+      "Stempel" = 1638437641;
+      "StopTheMadness" = 1376402589;
+      "TestFlight" = 899247664;
+      "WireGuard" = 1451685025;
+    };
   };
 }
