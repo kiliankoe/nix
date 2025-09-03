c81171bf9a7aa99f8c7da8aaf8fd9aabc9998d4c
Kilian Koeltzsch <me@kilian.io>
2025-09-03 14:31:07 +0200

    Update nhb and nhs aliases to use hostname -s on Darwin

--- home/darwin.nix
+++ home/darwin.nix
@@ -15,8 +15,8 @@
   programs.zsh = {
     initContent = ''
       alias brewup="brew upgrade && brew cleanup"
-      alias nhb="nh darwin build -H ${config.networking.hostName} ~/nix"
-      alias nhs="nh darwin switch -H ${config.networking.hostName} ~/nix"
+      alias nhb="nh darwin build -H $(hostname -s) ~/nix"
+      alias nhs="nh darwin switch -H $(hostname -s) ~/nix"
       # BSD ls flags
       alias ls='ls -G'
       alias l='ls -lAhG'
