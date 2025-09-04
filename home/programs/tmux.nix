084791dbf33b8dfe9200eb9731457515ef0ea7c5
Kilian Koeltzsch <me@kilian.io>
2025-09-04 09:50:32 +0200

    Refine tmux config and add plugins

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -1,40 +1,36 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
+    package = pkgs.tmux;
+    sensibleOnTop = false;
     mouse = true;
     prefix = "C-a";
     baseIndex = 1;
-    escapeTime = 0;
+    clock24 = true;
+
+    # These are taken care of by tmux-sensible, verify though before removing.
+    # escapeTime = 0;
+    # terminal = "screen-256color";
+    # aggressiveResize = true;
+    # historyLimit = 10000;
+    # keyMode = "emacs"; # Default, but explicit
 
     extraConfig = ''
-      set -g default-terminal "screen-256color"
-      setw -g aggressive-resize on
-
+      # Renumber windows when one is closed
       set -g renumber-windows on
 
-      set -g status-bg magenta
-      set -g status-fg black
-
-      set -g pane-active-border-style bg=default,fg=magenta
-      set -g pane-border-style fg=default
-
-      # rename terminals
-      set -g set-titles on
-
-      # more sensible pane splitting, \ doesn't require shift
+      # More sensible pane splitting, \ stands for | but doesn't require shift
       bind '\' split-window -h -c '#{pane_current_path}'
       bind - split-window -v -c '#{pane_current_path}'
       unbind '"'
       unbind %
 
-      # move windows with ctrl shift+arrow
+      # Move windows with ctrl shift+arrow
       bind-key S-Left swap-window -t -1 -d
       bind-key S-Right swap-window -t +1 -d
 
-      bind r source-file "$HOME/.config/tmux/tmux.conf" \; display-message "Reloaded config..."
-
-      # Remap window navigation to vim bindings
+      # Vim-style pane navigation
       unbind-key j
       bind-key j select-pane -D
       unbind-key k
@@ -44,10 +40,15 @@
       unbind-key l
       bind-key l select-pane -R
 
-      # Set the window title string
+      # Set title and disable automatic renaming
+      set -g set-titles on
       set-window-option -g automatic-rename off
 
-      # Active/current window styles
+      # Styling
+      set -g status-bg magenta
+      set -g status-fg black
+      set -g pane-active-border-style bg=default,fg=magenta
+      set -g pane-border-style fg=default
       set-window-option -g window-status-current-style "fg=black,bg=white,bold"
 
       # Format for active window: show directory name for zsh, command name otherwise, and Z if zoomed
@@ -58,6 +59,16 @@
 
       # Set terminal window title to reflect current window
       set-option -g set-titles-string "#I:#(if [ \"#{pane_current_command}\" = \"zsh\" ]; then basename \"#{pane_current_path}\" 2>/dev/null || echo \"?\"; else echo \"#{pane_current_command}\"; fi)#{?window_zoomed_flag, Z,}"
+
+      # Reload config on prefix-r - is this even still necessary with nix/home-manager?
+      bind r source-file ~/.config/tmux/tmux.conf \; display-message "Reloaded config..."
     '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      resurrect
+      continuum
+    ];
   };
 }
