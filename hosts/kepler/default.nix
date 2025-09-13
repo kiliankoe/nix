1cc1d2634aa2aef895a523bc95f67a6f815446d6
Kilian Koeltzsch <me@kilian.io>
2025-09-13 20:21:17 +0200

    comment out Tailscale WebDAV mount configuration

--- hosts/kepler/default.nix
+++ hosts/kepler/default.nix
@@ -101,26 +101,26 @@
   };
 
   # WebDAV mount for Tailscale
-  services.davfs2.enable = true;
-  fileSystems."/mnt/tailscale" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [ "defaults" ];
-  };
+  # services.davfs2.enable = true;
+  # fileSystems."/mnt/tailscale" = {
+  #   device = "none";
+  #   fsType = "tmpfs";
+  #   options = [ "defaults" ];
+  # };
 
-  systemd.mounts = [
-    {
-      enable = true;
-      description = "Tailscale Drive WebDAV mount";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      what = "http://100.100.100.100:8080";
-      where = "/mnt/tailscale";
-      options = "uid=1000,gid=100,file_mode=0664,dir_mode=0775,_netdev";
-      type = "davfs";
-      mountConfig.TimeoutSec = 30;
-    }
-  ];
+  # systemd.mounts = [
+  #   {
+  #     enable = true;
+  #     description = "Tailscale Drive WebDAV mount";
+  #     after = [ "network-online.target" ];
+  #     wants = [ "network-online.target" ];
+  #     what = "http://100.100.100.100:8080";
+  #     where = "/mnt/tailscale";
+  #     options = "uid=1000,gid=100,file_mode=0664,dir_mode=0775,_netdev";
+  #     type = "davfs";
+  #     mountConfig.TimeoutSec = 30;
+  #   }
+  # ];
 
   services.cron = {
     enable = true;
