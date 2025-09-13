bf37df40404f22abc1f03b186cefa0c1ab74a538
Kilian Koeltzsch <me@kilian.io>
2025-09-13 19:55:20 +0200

    migrate changedetection to docker-based service
    is kept more up to date than the nixpkg

--- services/changedetection.nix
+++ services/changedetection.nix
@@ -1,19 +1,50 @@
 { config, pkgs, ... }:
-{
-  services.changedetection-io = {
-    enable = true;
-    port = config.k.ports.changedetection_http;
-    listenAddress = "0.0.0.0";
+let
+  dockerService = import ../lib/docker-service.nix { inherit pkgs; };
 
-    playwrightSupport = true;
+  composeFile = pkgs.writeText "changedetection-compose.yml" ''
+    services:
+      changedetection:
+        image: ghcr.io/dgtlmoon/changedetection.io:latest
+        container_name: changedetection
+        hostname: changedetection
+        restart: unless-stopped
+        security_opt:
+          - "no-new-privileges:true"
+        environment:
+          - TZ=${config.time.timeZone}
+          - FETCH_WORKERS=10
+          - PLAYWRIGHT_DRIVER_URL=ws://sockpuppetbrowser:3000
+          - SCREEN_WIDTH=1920
+          - SCREEN_HEIGHT=1024
+        volumes:
+          - changedetection-data:/datastore
+        ports:
+          - '${toString config.k.ports.changedetection_http}:5000'
+        depends_on:
+          sockpuppetbrowser:
+            condition: service_started
 
-    datastorePath = "/var/lib/changedetection";
+      sockpuppetbrowser:
+        image: dgtlmoon/sockpuppetbrowser:latest
+        container_name: changedetection-browser
+        hostname: sockpuppetbrowser
+        restart: unless-stopped
+        security_opt:
+          - "no-new-privileges:true"
+        cap_add:
+          - SYS_ADMIN
+        environment:
+          - SCREEN_WIDTH=1920
+          - SCREEN_HEIGHT=1024
+          - SCREEN_DEPTH=16
+          - MAX_CONCURRENT_CHROME_PROCESSES=10
 
-    environmentFile = pkgs.writeText "changedetection-env" ''
-      TZ=${config.time.timeZone}
-      FETCH_WORKERS=10
-    '';
-  };
-
-  networking.firewall.allowedTCPPorts = [ config.k.ports.changedetection_http ];
+    volumes:
+      changedetection-data:
+  '';
+in
+dockerService.mkDockerComposeService {
+  serviceName = "changedetection";
+  composeFile = composeFile;
 }
