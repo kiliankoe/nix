21013eeabfd19067703547aad4bedee64b2bab8a
Kilian Koeltzsch <me@kilian.io>
2025-10-20 11:27:52 +0200

    add missing mato/kagi_api_key definition on kepler

--- hosts/kepler/default.nix
+++ hosts/kepler/default.nix
@@ -60,6 +60,7 @@
     "mato/smtp_host" = { };
     "mato/smtp_pass" = { };
     "mato/smtp_user" = { };
+    "mato/kagi_api_key" = { };
     "paperless/secret_key" = { };
     "swiftdebot/discord_app_id" = { };
     "swiftdebot/discord_logs_webhook_url" = { };
