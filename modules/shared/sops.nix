82a65702a5a2d4204dd2c3f1d7dc3a35fb22af03
Kilian Koeltzsch <me@kilian.io>
2025-11-11 09:06:38 +0100

    silence sops warnings about missing ssh keys

--- modules/shared/sops.nix
+++ modules/shared/sops.nix
@@ -13,4 +13,9 @@
     else
       "/home/kilian/.config/sops/age.key";
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
+
+  # Disable SSH key usage since we're using age encryption only
+  # This silences warnings about missing SSH keys
+  sops.age.sshKeyPaths = [ ];
+  sops.gnupg.sshKeyPaths = [ ];
 }
