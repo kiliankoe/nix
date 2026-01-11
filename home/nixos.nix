feef0cecea995a531fe9de642a1a05728cd9f73d
Kilian Koeltzsch <me@kilian.io>
2026-01-11 22:06:07 +0100

    remove unnecessary kepler-deploy alias

--- home/nixos.nix
+++ home/nixos.nix
@@ -16,7 +16,6 @@
     initContent = ''
       alias nhb="nh os build -H ${osConfig.networking.hostName} ~/nix"
       alias nhs="nh os switch -H ${osConfig.networking.hostName} ~/nix"
-      alias kepler-deploy='nix run github:serokell/deploy-rs -- ~/nix#kepler'
       alias ls='ls --color=auto'
       alias l='ls -lAh --color=auto'
     '';
