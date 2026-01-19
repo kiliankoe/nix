0d31c204f5a6472c6d9aeb65947f41230a08e3a3
Kilian Koeltzsch <me@kilian.io>
2026-01-19 22:02:01 +0100

    add ghcr.io auth to watchtower

--- services/watchtower.nix
+++ services/watchtower.nix
@@ -1,6 +1,12 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
   dockerService = import ../lib/docker-service.nix { inherit pkgs lib; };
+  serviceDir = "/etc/docker-compose/watchtower";
 in
 dockerService.mkDockerComposeService {
   serviceName = "watchtower";
@@ -11,8 +17,33 @@ dockerService.mkDockerComposeService {
       image = "nicholas-fedor/watchtower";
       container_name = "watchtower";
       restart = "unless-stopped";
-      volumes = [ "/var/run/docker.sock:/var/run/docker.sock" ];
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+        "${serviceDir}/config.json:/config.json:ro"
+      ];
       command = "--label-enable --interval 21600"; # 6 hours
     };
+  };
+}
+// {
+  sops.secrets."watchtower/ghcr_auth" = { };
+
+  systemd.services.watchtower = {
+    serviceConfig.ExecStartPre = lib.mkBefore [
+      (pkgs.writeScript "watchtower-generate-config" ''
+        #!${pkgs.bash}/bin/bash
+        AUTH=$(cat ${config.sops.secrets."watchtower/ghcr_auth".path})
+        cat > ${serviceDir}/config.json <<EOF
+        {
+          "auths": {
+            "ghcr.io": {
+              "auth": "$AUTH"
+            }
+          }
+        }
+        EOF
+        chmod 600 ${serviceDir}/config.json
+      '')
+    ];
   };
 }
