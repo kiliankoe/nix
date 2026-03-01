a693f591e313cbb02a2442b14290f5cc3cbeec3e
Kilian Koeltzsch <me@kilian.io>
2026-03-01 11:32:09 +0100

    set sab inet_exposure

--- hosts/kepler/services/sabnzbd.nix
+++ hosts/kepler/services/sabnzbd.nix
@@ -20,6 +20,7 @@
     settings.misc = {
       port = config.k.ports.sabnzbd_http;
       host = "0.0.0.0";
+      inet_exposure = "api+web (locally no auth)";
       download_dir = "/mnt/media/download/incomplete";
       complete_dir = "/mnt/media/download/complete";
     };
