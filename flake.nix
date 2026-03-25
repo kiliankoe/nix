3ceefb197cd1b3e0eac30cd8ecf1929e127d0bb3
Kilian Koeltzsch <me@kilian.io>
2026-03-25 10:12:46 +0100

    pin nixpkgs to nixos-unstable instead of nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Unified Nix Configuration for Multiple Machines";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
