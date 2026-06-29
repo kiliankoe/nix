9e244020d42da87a0c0145fbd2c76630c2321952
Kilian Koeltzsch <me@kilian.io>
2026-06-29 13:46:55 +0200

    wbbash: migrate to new miniqdb

--- hosts/kepler/services/docker/wbbash.nix
+++ hosts/kepler/services/docker/wbbash.nix
@@ -10,7 +10,7 @@ in
 dockerService.mkDockerComposeService {
   serviceName = "wbbash";
   auto_update = true;
-  backupVolumes = [ "wbbash-db" ];
+  backupVolumes = [ "wbbash-pb-data" ];
   monitoring.httpEndpoint = {
     name = "wbbash";
     url = "http://localhost:${toString config.k.ports.wbbash_http}/";
@@ -18,29 +18,40 @@ dockerService.mkDockerComposeService {
   compose = {
     services.wbbash = {
       container_name = "wbbash";
-      image = "ghcr.io/kiliankoe/wbbash:main";
+      image = "ghcr.io/kiliankoe/miniqdb:main";
       restart = "unless-stopped";
-      environment = [
-        "DATABASE_URL=file:/data/db.sqlite"
-      ];
       env_file = [ "wbbash.env" ];
-      volumes = [ "wbbash-db:/data" ];
-      ports = [ "${toString config.k.ports.wbbash_http}:3000" ];
+      depends_on = [ "pocketbase" ];
+      ports = [ "${toString config.k.ports.wbbash_http}:80" ];
     };
-    volumes.wbbash-db = { };
+    # This service name MUST be `pocketbase`.
+    services.pocketbase = {
+      container_name = "wbbash-pocketbase";
+      image = "ghcr.io/kiliankoe/miniqdb-pocketbase:main";
+      restart = "unless-stopped";
+      env_file = [ "pocketbase.env" ];
+      volumes = [ "wbbash-pb-data:/pb/pb_data" ];
+      # Localhost-only: admin UI + data migration
+      ports = [ "127.0.0.1:8090:8090" ];
+    };
+    volumes.wbbash-pb-data = { };
   };
   environment = {
     wbbash = {
-      MINIQDB_NAME.secret = "wbbash/miniqdb_name";
+      APP_NAME.secret = "wbbash/miniqdb_name";
+      LOGIN_BUTTON_TEXT.secret = "wbbash/login_button_text";
+      NOTHING_TO_SEE_HERE_BUTTON_TEXT.secret = "wbbash/nothing_to_see_here_text";
+    };
+    pocketbase = {
       ALLOWED_DOMAINS.secret = "wbbash/allowed_domains";
-      NEXT_PUBLIC_NOTHING_TO_SEE_HERE_BUTTON_TEXT.secret = "wbbash/nothing_to_see_here_text";
-      NEXT_PUBLIC_LOGIN_BUTTON_TEXT.secret = "wbbash/login_button_text";
-      NEXTAUTH_SECRET.secret = "wbbash/nextauth_secret";
-      EMAIL_SERVER_HOST.secret = "wbbash/email_server_host";
-      EMAIL_SERVER_PORT.secret = "wbbash/email_server_port";
-      EMAIL_SERVER_USER.secret = "wbbash/email_server_user";
-      EMAIL_SERVER_PASSWORD.secret = "wbbash/email_server_password";
-      EMAIL_FROM.secret = "wbbash/email_from";
+      ADMIN_EMAILS.secret = "wbbash/admin_emails";
+      SMTP_HOST.secret = "wbbash/email_server_host";
+      SMTP_PORT.secret = "wbbash/email_server_port";
+      SMTP_USERNAME.secret = "wbbash/email_server_user";
+      SMTP_PASSWORD.secret = "wbbash/email_server_password";
+      SMTP_SENDER_ADDRESS.secret = "wbbash/email_from";
+      SMTP_TLS = "true";
+      SMTP_SENDER_NAME = "wbbash";
     };
   };
 }
