ab2a66db87909d252444d487d7306ae2238abc53
Kilian Koeltzsch <me@kilian.io>
2026-07-07 08:59:30 +0200

    add hister cli to darwin systems

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 {
   environment.systemPackages = with pkgs; [
     _1password-cli
     dedup-darwin
     terminal-notifier
+
+    inputs.hister.packages.${pkgs.system}.default
 
     # inputs.npr.packages.${pkgs.system}.default
   ];
