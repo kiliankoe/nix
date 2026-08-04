97d72fc3fe8587ae55355effdf430213f5cdd201
Kilian Koeltzsch <me@kilian.io>
2026-08-04 13:55:40 +0200

    cassini: install some tools globally

--- hosts/cassini/packages.nix
+++ hosts/cassini/packages.nix
@@ -2,12 +2,17 @@
 {
   environment.systemPackages = with pkgs; [
     acli
+    azure-cli
     delta
     devenv
     emcee
     glab
     k9s
     kubectl
+    kubelogin
+    kubernetes-helm
+    kustomize
+    natscli
     pnpm
   ];
 }
